4c609d5b92aff869c3e49037a964490706cfa4bc
NetLSS <futureprogrammer@naver.com>
2021-08-24 18:58:26 +0900

    Feat: 브로드캐스트 수신자 등록(register) 하기

--- SendBroadCast/app/src/main/java/com/lilcode/example/sendbroadcast/MainActivity.kt
+++ SendBroadCast/app/src/main/java/com/lilcode/example/sendbroadcast/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.lilcode.example.sendbroadcast
 
+import android.content.BroadcastReceiver
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    var receiver: BroadcastReceiver? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configureReceiver()
+    }
+
+    // 리시버에 수신할 액션 필터를 추가함.
+    private fun configureReceiver() {
+        val filter = IntentFilter()
+        filter.addAction("com.lilcode.example.sendbroadcast")
+        receiver = MyReceiver()
+        registerReceiver(receiver, filter)
     }
 
     // 브로드캐스트 인텐트 생성 및 전송
@@ -17,5 +31,10 @@ class MainActivity : AppCompatActivity() {
         intent.action = "com.lilcode.example.sendbroadcast" // 수신자에서는 해당 액션과 일치하는 액션 필요.
         intent.flags = Intent.FLAG_INCLUDE_STOPPED_PACKAGES
         sendBroadcast(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
